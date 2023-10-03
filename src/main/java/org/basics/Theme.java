f0ba4f0eb843a037e7e839cf4ef369c913b1c370
NotHenry <henryeichten@gmail.com>
2023-10-03 16:41:02 -0500

    Implemented start of Theme system to allow standard rules across all visuals

---
+++ src/main/java/org/basics/Theme.java
@@ -0,0 +1,13 @@
+package org.basics;
+
+import java.io.File;
+
+public class Theme {
+    public Theme() {
+        loadConfig();
+    }
+
+    private void loadConfig() {
+        File config = new File("");
+    }
+}
