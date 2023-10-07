5a21b5a7932bc6f997b54d2b9270672a7c935b5b
NotHenry <henryeichten@gmail.com>
2023-10-07 10:29:35 -0500

    Revert changes on theme

--- src/main/java/org/main/Theme.java
+++ src/main/java/org/main/Theme.java
@@ -1,6 +1,7 @@
 package org.main;
 
-import org.json.simple.JSONObject;
+
+import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
@@ -8,6 +9,9 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Theme {
+    private static org.json.simple.JSONObject simpleJSONObject;
+    private static org.json.JSONObject JSONObject;
+
     public static void main(String[] args) throws IOException, ParseException {
         loadConfig();
     }
@@ -15,17 +19,16 @@ public class Theme {
         loadConfig();
     }
 
-    private static void loadConfig() throws IOException, ParseException {
+    private static void loadConfig() throws IOException {
         // TODO: Put each json info into respective global variables
 
         JSONParser parser = new JSONParser();
         try {
             Object configObject = parser.parse(new FileReader(".\\src\\main\\resources\\theme_config.json"));
 
-            JSONObject config =  (JSONObject) configObject;
-            System.out.println(config.toString());
         } catch (ParseException e) {
             System.out.println("MISSING FILE: theme_config.json");
         }
+
     }
 }
