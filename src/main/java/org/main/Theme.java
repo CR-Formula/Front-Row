ebf1c1075370a624bb19495ff0402a1602997bb9
NotHenry <henryeichten@gmail.com>
2023-10-09 13:06:21 -0500

    Update Theme

--- src/main/java/org/main/Theme.java
+++ src/main/java/org/main/Theme.java
@@ -5,22 +5,23 @@ import com.google.gson.annotations.Expose;
 import java.awt.*;
 
 public class Theme {
-
-    // Font info
-    public final static String fontName = "Courier";
-    public final static int graphPadding = 10;
-    public final static int largeFontSize = 16;
-    public final static int normalFontSize = 12;
-    public final static int smallFontSize = 8;
-
-    // Lines
-    public final static int smallLineWidth = 1;
-    public final static int mediumLineWidth = 3;
-    public final static int largeLineWidth = 5;
+    // Paddings
+    public static int graphPadding = 10;
 
     // Colors
     public final static Color pageBackground = new Color(0x504f61);
     public final static Color graphBackground = new Color(0x000000);
     public final static Color graphBorder = new Color(0xFFFFFF);
     public final static Color graphInfo = new Color(0xFFD677);
+
+    // Font details
+    public final static String fontName = "Courier";
+    public final  static Font largeFont = new Font(fontName, Font.PLAIN, 16);
+    public final  static Font normalFont = new Font(fontName, Font.PLAIN, 12);
+    public final  static Font smallFont = new Font(fontName, Font.PLAIN, 8);
+
+    // Line details
+    public final  static int smallLineWidth = 1;
+    public final  static int mediumLineWidth = 3;
+    public final  static int largeLineWidth = 15;
 }
