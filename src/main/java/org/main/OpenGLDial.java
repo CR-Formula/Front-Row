4aedaf0391689fcad7581327017ea67baef03000
NotHenry <henryeichten@gmail.com>
2024-03-02 14:22:04 -0600

    OpenGLDial converted

--- src/main/java/org/main/OpenGLDial.java
+++ src/main/java/org/main/OpenGLDial.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 
 public class OpenGLDial extends SecondaryGraph implements GLEventListener {
-    private FloatBuffer verticies;
+    private float[] vertices;
     private float maxValue;
     private float minValue;
     private final int sampleCount = 1000;
@@ -18,7 +18,6 @@ public class OpenGLDial extends SecondaryGraph implements GLEventListener {
     public OpenGLDial() {
         this.maxValue = 1;
         this.minValue = -1;
-        verticies = Buffers.newDirectFloatBuffer(sampleCount * 4 * Buffers.SIZEOF_FLOAT);
     }
 
     public void setMaxMin(float maxValue, float minValue) {
@@ -28,7 +27,12 @@ public class OpenGLDial extends SecondaryGraph implements GLEventListener {
 
     @Override
     public void init(GLAutoDrawable glAutoDrawable) {
-        super.setupOpenGL(glAutoDrawable);
+        GL2ES3 gl = glAutoDrawable.getGL().getGL2ES3();
+//        super.setupOpenGL(glAutoDrawable);
+        OpenGL.makeAllPrograms(gl);
+        OpenGL.makeOrthoMatrix(screenMatrix, -1, 1, -1, 1, -10000, 10000);
+        OpenGL.useMatrix(gl, screenMatrix);
+        setVertices(glAutoDrawable);
     }
 
     @Override
@@ -39,7 +43,7 @@ public class OpenGLDial extends SecondaryGraph implements GLEventListener {
             GL2ES3 gl = glAutoDrawable.getGL().getGL2ES3();
 
             float[] backgroundColor = new float[]{Theme.graphBackground.getRed() / 255.0f, Theme.graphBackground.getGreen() / 255.0f, Theme.graphBackground.getBlue() / 255.0f, 1.0f};
-            OpenGL.drawBox(gl, backgroundColor, leftPlotX, bottomPlotY, rightPlotX - leftPlotX, topPlotY - bottomPlotY);
+            OpenGL.drawBox(gl, backgroundColor, -1, -1, 1 - -1, 1 - -1);
 
             float value = dataset.hasValues() ? dataset.getLastSample() : minValue;
             if (autoDetectMaxMin && value > maxValue) maxValue = value;
@@ -50,11 +54,10 @@ public class OpenGLDial extends SecondaryGraph implements GLEventListener {
             }
 
             float percent = Math.max(Math.min(1f, (value - minValue) / (maxValue - minValue)), 0);
-            float[] datasetColor = new float[]{dataset.getColor().getRed() / 255.0f, dataset.getColor().getGreen() / 255.0f, dataset.getColor().getBlue() / 255.0f, 1f};
-
+            float[] datasetColor = new float[]{1f, 0f, 0f, 1f};
             OpenGL.buffer.rewind();
-            setVertices(glAutoDrawable);
-            OpenGL.drawTrianglesXY(gl, GL3.GL_TRIANGLE_STRIP, datasetColor, OpenGL.buffer, (int)(sampleCount * percent));
+            OpenGL.buffer.put(vertices);
+            OpenGL.drawTrianglesXY(gl, GL3.GL_TRIANGLE_STRIP, datasetColor, OpenGL.buffer, (int)(percent * sampleCount));
             OpenGL.buffer.rewind();
         } catch (Exception e) {
             System.out.println("Cannot draw dataset");
@@ -63,25 +66,31 @@ public class OpenGLDial extends SecondaryGraph implements GLEventListener {
 
     private void setVertices(GLAutoDrawable glAutoDrawable) {
         // Set current dimensions
+        vertices = new float[(int)(sampleCount * 2)];
         graphWidth = glAutoDrawable.getSurfaceWidth();
         graphHeight = glAutoDrawable.getSurfaceHeight();
 
         final float desiredRadius = 0.7f;
         final float[] radiusOuter = {
-                graphWidth < graphHeight ? desiredRadius : graphHeight / graphWidth * desiredRadius,
+                (graphWidth < graphHeight) ? desiredRadius : graphHeight / graphWidth * desiredRadius,
                 graphHeight < graphWidth ? desiredRadius : graphWidth / graphHeight * desiredRadius
         };
 
-        final float[] radiusInner = {radiusOuter[0] - (0.25f * radiusOuter[0]), radiusOuter[1] - (0.25f * radiusOuter[1])};
+        final float[] radiusInner = {radiusOuter[0] - (0.25f * radiusOuter[0]),
+                radiusOuter[1] - (0.25f * radiusOuter[1])};
 
         float originX = 0;
         float originY = radiusOuter[1] / -2f;
         float increment = ((float) Math.PI / ((float) sampleCount / 2));
+        int i = 0;
         for (double angle = 0; angle < Math.PI; angle += increment) {
-            OpenGL.buffer.put(-1 * (originX + ((float) Math.cos(angle) * radiusInner[0])));
-            OpenGL.buffer.put(originY + ((float)Math.sin(angle) * radiusInner[1]));
-            OpenGL.buffer.put(-1 * (originX + ((float) Math.cos(angle) * radiusOuter[0])));
-            OpenGL.buffer.put(originY + ((float)Math.sin(angle) * radiusOuter[1]));
+            vertices[i]     = (-1 * (originX + ((float) Math.cos(angle) * radiusInner[0])));
+            vertices[i + 1] = (originY + ((float)Math.sin(angle) * radiusInner[1]));
+            vertices[i + 2] = (-1 * (originX + ((float) Math.cos(angle) * radiusOuter[0])));
+            vertices[i + 3] = (originY + ((float)Math.sin(angle) * radiusOuter[1]));
+            i += 4;
+            System.out.println((-1 * (originX + ((float) Math.cos(angle) * radiusInner[0]))) + " | " + (originY + ((float)Math.sin(angle) * radiusInner[1])) + " | "
+                    + (-1 * (originX + ((float) Math.cos(angle) * radiusOuter[0]))) + " | " + (originY + ((float)Math.sin(angle) * radiusOuter[1])));
         }
     }
 
